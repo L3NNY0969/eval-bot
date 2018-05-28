207f88cd28f13d7d67b5ebffd932552fc41f1e06
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-28 11:56:21 -0500

    more fix

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ async def ping(ctx):
 async def _eval(ctx, *, body: str):
     if "bot.ws.token" in body or "os.environ.get('TOKEN')" in body or 'os.environ.get("TOKEN")' in body:
         return await ctx.send("No token for you.")
-    lol = bot.get_channel(408030365773463562)
+    lol = bot.get_channel(450703273050243083)
     await lol.send(f"**{ctx.message.author.name}** has run the code: \n\n```{body}```")        
     env = {
         'bot': bot,
